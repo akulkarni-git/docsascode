539c7595a41e06d38da040529b7693fcaf6e4d22
akulkarni-git <aparna.a.saxena@gmail.com>
2026-09-10 14:59:13 +0530

    final

--- company-docs/docusaurus.config.js
+++ company-docs/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Aparna K Site',
+  tagline: 'Dino is cool',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
